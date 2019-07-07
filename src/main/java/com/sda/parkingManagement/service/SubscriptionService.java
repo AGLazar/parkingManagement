d8095a2cf86b8ecc555d780f91356b6c8a8f62af
Octavian <octavian.ciobanuca@gmail.com>
2019-07-07 12:13:29 +0300

    addSubscriptionComplet

---
+++ src/main/java/com/sda/parkingManagement/service/SubscriptionService.java
@@ -0,0 +1,11 @@
+package com.sda.parkingManagement.service;
+
+import com.sda.parkingManagement.repository.SubscriptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SubscriptionService {
+    @Autowired
+    public SubscriptionRepository subscriptionRepository;
+}
